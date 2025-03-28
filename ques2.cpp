0db8f70ff04d8dc46979a4c390f84ad9dc99b752
HarshMohanSahay <harshmohansahay@gmail.com>
2025-03-28 20:05:29 +0530

    my second question

---
+++ ques2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter the length of rectangle ";
+    cin>>a;
+    cout<<"enter the breath of rectangle ";
+    cin>>b;
+    cout<<"this is the area of rectangle ";
+    cout<<a*b;
+}
---
+++ ques3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter the first number";
+    cin>>a;
+    cout<<"enter the second number";
+    cin>>b;
+    cout<<"this is the addition ";
+    cout<<a+b<<endl;
+    cout<<"this is the substraction ";
+    cout<<a-b<<endl;
+    cout<<"this is the multiplication ";
+    cout<<a*b<<endl;
+    cout<<"this is the division "; 
+    cout<<a/b;
+}
